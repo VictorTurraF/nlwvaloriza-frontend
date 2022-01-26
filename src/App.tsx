2a83265adb04a6b39668c15209aab09adf41c435
Victor Turra <victor.florencio@univem.edu.br>
2022-01-25 23:26:19 -0300

    refactor: Restructure routes paths and add empty pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Spinner from "./components/Spinner";
 
-const DashboardPage = React.lazy(() => import("./pages/Dashboard"));
+const DashboardLayout = React.lazy(() => import("./layouts/DashboardLayout"));
 const LoginPage = React.lazy(() => import("./pages/Login"));
+const ComplimentsPage = React.lazy(() => import("./pages/Compliments"));
+const TagsPage = React.lazy(() => import("./pages/Tags"));
+const UsersPage = React.lazy(() => import("./pages/Users"));
+const AddNewTagPage = React.lazy(() => import("./pages/AddNewTagPage"));
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<Spinner />}>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="panel" element={<DashboardLayout />}>
+            <Route path="users" element={<UsersPage />} />
+            <Route path="compliments" element={<ComplimentsPage />} />
+            <Route path="tags">
+              <Route path="new" element={<AddNewTagPage />} />
+              <Route index element={<TagsPage />} />
+            </Route>
+          </Route>
         </Routes>
       </Suspense>
     </BrowserRouter>
