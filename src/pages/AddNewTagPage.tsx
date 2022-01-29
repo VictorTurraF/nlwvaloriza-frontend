95d90f897362d2f041eef1c1cc6b3caed6390334
Victor Turra <victor.florencio@univem.edu.br>
2022-01-29 18:48:36 -0300

    feat: Implement create tag page

--- src/pages/AddNewTagPage.tsx
+++ src/pages/AddNewTagPage.tsx
@@ -1,10 +1,31 @@
-import { Space, Typography } from "antd";
+import { message, Space, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
 import TagForm from "../components/TagForm";
+import * as tagsClient from "../http/tags";
+
 
 function AddNewTagPage() {
+  const navigate = useNavigate()
 
-  function handleSubmit (values: any) {
-    console.log(values);
+  async function requestTagCreation (tag: tagsClient.TagCreateRequest) {
+    try {
+      const response = await tagsClient.create(tag)
+      return response
+    } catch (error) {
+      console.warn('Erro ao criar tags')
+      console.warn(error)
+      return null
+    }
+  }
+
+  async function handleSubmit (values: any) {
+    const response = await requestTagCreation(values);
+    if (!!response && response.status === 201) {
+      message.success('Tag successfuly created');
+      navigate('/panel/tags');
+    }
+
+
   }
 
   return (
