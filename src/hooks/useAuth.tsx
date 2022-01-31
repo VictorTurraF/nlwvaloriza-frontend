1747e41534b14e66f3433f7106f4881aaacb9152
Victor Turra <victor.florencio@univem.edu.br>
2022-01-31 03:36:21 -0300

    refactor: Simplify hook code

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,6 +2,5 @@ import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export function useAuth () {
-  const context = useContext(AuthContext);
-  return context
+  return useContext(AuthContext);
 }
