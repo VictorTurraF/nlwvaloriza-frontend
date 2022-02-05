0ce500c9720b31fab59b117871f848ec89fea330
Victor Turra <victor.florencio@univem.edu.br>
2022-02-05 11:56:59 -0300

    feat: Implement my profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,10 +1,21 @@
-import { Space, Typography } from "antd";
+import { Badge, Descriptions, Space, Tag, Typography } from "antd";
+import { useAuth } from "../hooks/useAuth";
 
 function ProfilePage() {
+  const { user } = useAuth();
   return (
     <Space direction="vertical">
       <Typography.Title level={4}>My Profile</Typography.Title>
-      <Typography.Paragraph>Profile informations</Typography.Paragraph>
+      <Descriptions title="User Info" bordered>
+        <Descriptions.Item label="Name">{user?.name}</Descriptions.Item>
+        <Descriptions.Item label="Email">{user?.email}</Descriptions.Item>
+        <Descriptions.Item label="Access Level">
+          {user?.is_admin ? <Tag color="gold">admin</Tag> : <Tag>user</Tag>}
+        </Descriptions.Item>
+        <Descriptions.Item label="Status">
+          <Badge status="success" text="Codding" />
+        </Descriptions.Item>
+      </Descriptions>
     </Space>
   );
 }
