0d8bc0c6553c44f48c30e84d29d3bf414f7e291c
Victor Turra <victor.florencio@univem.edu.br>
2022-02-05 12:10:26 -0300

    refactor: Enhance component types hierachy

--- src/types/components.ts
+++ src/types/components.ts
@@ -3,11 +3,11 @@ import React from "react";
 /**
  * For default properties like children, className, etc
  */
-export interface HTMLElement extends React.HTMLProps<any> {};
+export interface HTMLElement<T = any> extends React.HTMLProps<T> {};
 
-export interface DivElement extends React.HTMLProps<HTMLDivElement> {};
-export interface FormElement extends React.HTMLProps<HTMLFormElement> {};
-export interface ButtonElement extends React.HTMLProps<HTMLButtonElement> {};
-export interface SelectElement extends React.HTMLProps<HTMLSelectElement> {};
-export interface InputElement extends React.HTMLProps<HTMLInputElement> {};
-export interface FormControlsElement extends React.HTMLProps<HTMLFormControlsCollection> {};
+export interface DivElement extends HTMLElement<HTMLDivElement> {};
+export interface FormElement extends HTMLElement<HTMLFormElement> {};
+export interface ButtonElement extends HTMLElement<HTMLButtonElement> {};
+export interface SelectElement extends HTMLElement<HTMLSelectElement> {};
+export interface InputElement extends HTMLElement<HTMLInputElement> {};
+export interface FormControlsElement extends HTMLElement<HTMLFormControlsCollection> {};
