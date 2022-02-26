0261a679d0178d71485d91663ac45816145b9367
Victor Turra <victor.florencio@univem.edu.br>
2022-02-26 17:18:17 -0300

    chore: Implement debouncer hook

---
+++ src/hooks/useDebouce.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export interface DebouncerConfig<T> {
+  valueToBeDebounced: T;
+  onTimeOut: (value: T) => void;
+  timeoutInSeconds: number;
+}
+
+export function useDebouce<T = any>(config: DebouncerConfig<T>) {
+  const { valueToBeDebounced, onTimeOut, timeoutInSeconds } = config;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onTimeOut(valueToBeDebounced);
+    }, timeoutInSeconds * 1000);
+
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [valueToBeDebounced, onTimeOut, timeoutInSeconds]);
+}
+
+export default useDebouce;
