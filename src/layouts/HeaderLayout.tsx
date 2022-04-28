b16e78ba9134b02973e510acf5f3e23ef913e2d0
Victor Turra <victor.florencio@univem.edu.br>
2022-04-28 00:15:42 -0300

    style: Add loggout button in header

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,18 +1,15 @@
 import * as React from "react";
-import { Layout } from "antd";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from "@ant-design/icons";
+import { Button, Col, Layout, Row } from "antd";
+import { MenuUnfoldOutlined, MenuFoldOutlined, LogoutOutlined } from "@ant-design/icons";
+import { DivElement } from "../types/components";
 const { Header } = Layout;
 
-export interface HeaderLayoutProps {
-  collapsed: boolean,
-  toggle: any
+export interface HeaderLayoutProps extends DivElement {
+  collapsed: boolean;
+  toggle: any;
 }
 
-function HeaderLayout({ collapsed, toggle }: HeaderLayoutProps) {
-
+function HeaderLayout({ collapsed, toggle, children }: HeaderLayoutProps) {
   const menuConfig = {
     className: "trigger",
     onClick: toggle,
@@ -20,11 +17,20 @@ function HeaderLayout({ collapsed, toggle }: HeaderLayoutProps) {
 
   return (
     <Header className="site-layout-background" style={{ padding: 0 }}>
-      {collapsed ? (
-        <MenuUnfoldOutlined {...menuConfig} />
-      ) : (
-        <MenuFoldOutlined {...menuConfig} />
-      )}
+      <Row>
+        <Col flex="auto">
+          {collapsed ? (
+            <MenuUnfoldOutlined {...menuConfig} />
+          ) : (
+            <MenuFoldOutlined {...menuConfig} />
+          )}
+        </Col>
+        <Col>
+          <Button icon={<LogoutOutlined />} style={{ marginRight: "1rem" }} type="primary">
+            Logout
+          </Button>
+        </Col>
+      </Row>
     </Header>
   );
 }
