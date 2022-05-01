a330d5a08c8ed07dd5e087fbc07926a0dc9a6911
Victor Turra <victor.florencio@univem.edu.br>
2022-05-01 10:22:34 -0300

    feat: Add loggout button in panel header

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Button, Col, Layout, Row } from "antd";
 import { MenuUnfoldOutlined, MenuFoldOutlined, LogoutOutlined } from "@ant-design/icons";
 import { DivElement } from "../types/components";
+import { useAuth } from "../hooks/useAuth";
 const { Header } = Layout;
 
 export interface HeaderLayoutProps extends DivElement {
@@ -15,6 +16,14 @@ function HeaderLayout({ collapsed, toggle, children }: HeaderLayoutProps) {
     onClick: toggle,
   };
 
+  const auth = useAuth()
+
+  function handleLoggoutClick(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    auth.signOut();
+  }
+
+
   return (
     <Header className="site-layout-background" style={{ padding: 0 }}>
       <Row>
@@ -26,7 +35,12 @@ function HeaderLayout({ collapsed, toggle, children }: HeaderLayoutProps) {
           )}
         </Col>
         <Col>
-          <Button icon={<LogoutOutlined />} style={{ marginRight: "1rem" }} type="primary">
+          <Button
+            icon={<LogoutOutlined />}
+            style={{ marginRight: "1rem" }}
+            type="primary"
+            onClick={handleLoggoutClick}
+          >
             Logout
           </Button>
         </Col>
